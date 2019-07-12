b8535a083f81a7e2ecc4b1dcd63607eed2a3e723
romanstetsiuk1 <romansteciuk021@gmail.com>
2019-07-12 14:25:46 +0200

    Get fileList from directory

---
+++ src/main/java/App.java
@@ -0,0 +1,11 @@
+import java.io.File;
+
+public class App {
+
+    public static void main(String[] args) {
+
+        File directory = new File("C:\\Users\\roman.stetsiuk\\Documents\\VK\\XTB_raports");
+        File[] filesList = directory.listFiles();
+        
+    }
+}
