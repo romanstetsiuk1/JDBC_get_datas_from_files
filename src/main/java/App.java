f793cea3d3d03bc11a159bdd6eb2f194c6c06ab2
Roman Stetsiuk <romansteciuk021@gmail.com>
2019-08-03 16:35:15 +0200

    Create interface and new class by create tables in MySQL

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -21,82 +21,17 @@ public class App {
         try (Connection connection = DriverManager.getConnection(connectionUrl, userName, password);
              Statement statement = connection.createStatement()) {
 
-            statement.executeUpdate("CREATE TABLE IF NOT EXISTS accountBalance(" +
-                    "accountBalance_id INT NOT NULL AUTO_INCREMENT," +
-                    "accountBalance_date VARCHAR(12)," +
-                    "balance VARCHAR(12)," +
-                    "equity VARCHAR(12)," +
-                    "margin VARCHAR(12)," +
-                    "freeMargin VARCHAR(12)," +
-                    "PRIMARY KEY (accountBalance_id))");
-
-            statement.executeUpdate("CREATE TABLE IF NOT EXISTS closedTransactions(" +
-                    "closedTransaction_id INT NOT NULL AUTO_INCREMENT," +
-                    "raportDate VARCHAR(12)," +
-                    "ticket VARCHAR(15)," +
-                    "openTimeTransactions VARCHAR(25)," +
-                    "typeTransactions VARCHAR(5)," +
-                    "lots VARCHAR(5)," +
-                    "symbol VARCHAR(10)," +
-                    "exchangeCode VARCHAR(5)," +
-                    "assetClass VARCHAR(20)," +
-                    "openPrice VARCHAR(10)," +
-                    "closeTime VARCHAR(25)," +
-                    "closePrise VARCHAR(10)," +
-                    "conversionRate VARCHAR(15)," +
-                    "commissions VARCHAR(10)," +
-                    "swap VARCHAR(10)," +
-                    "profit VARCHAR(10)," +
-                    "PRIMARY  KEY (closedTransaction_id))");
-
-
-            statement.executeUpdate("CREATE TABLE IF NOT EXISTS openTransactions(" +
-                    "openTransactions_id INT NOT NULL AUTO_INCREMENT," +
-                    "raportDate VARCHAR(12)," +
-                    "ticket VARCHAR(15)," +
-                    "openTimeTransactions VARCHAR(25)," +
-                    "typeTransactions VARCHAR(5)," +
-                    "lots VARCHAR(5)," +
-                    "symbol VARCHAR(10)," +
-                    "exchangeCode VARCHAR(5)," +
-                    "assetClass VARCHAR(20)," +
-                    "openPrice VARCHAR(10)," +
-                    "marketPrise VARCHAR(10)," +
-                    "conversionRate VARCHAR(15)," +
-                    "commissions VARCHAR(10)," +
-                    "swap VARCHAR(10)," +
-                    "profit VARCHAR(10)," +
-                    "PRIMARY  KEY (openTransactions_id))");
-
-            statement.executeUpdate("CREATE TABLE  IF NOT EXISTS depositsWithdrawals(" +
-                    "depositsWithdrawals_id INT NOT NULL AUTO_INCREMENT," +
-                    "raportDate VARCHAR(12)," +
-                    "ticket VARCHAR(12)," +
-                    "openTime VARCHAR(25)," +
-                    "typeOperation VARCHAR(15)," +
-                    "comment VARCHAR(150)," +
-                    "deposit VARCHAR(10)," +
-                    "withdraw VARCHAR(10)," +
-                    "netDeposit VARCHAR(10)," +
-                    "PRIMARY KEY (depositsWithdrawals_id))");
-
-            statement.executeUpdate("CREATE TABLE  IF NOT EXISTS totalValues(" +
-                    "totalValues_id INT NOT NULL AUTO_INCREMENT," +
-                    "raportDate VARCHAR(12)," +
-                    "closedTransactions_commission VARCHAR(10)," +
-                    "closedTransactions_swap VARCHAR(10)," +
-                    "closedTransactions_profit VARCHAR(10)," +
-                    "closedTransactions_closedTrade VARCHAR(10)," +
-                    "openTransactions_commission VARCHAR(10)," +
-                    "openTransactions_swap VARCHAR(10)," +
-                    "openTransactions_profit VARCHAR(10)," +
-                    "openTransactions_floating VARCHAR(10)," +
-                    "depositWithdrawal VARCHAR(10)," +
-                    "PRIMARY KEY (totalValues_id))");
+            CreteTableInMySql createTables = new CreteTableInMySql();
+            createTables.createTableAccountBalance(statement);
+            createTables.createTableTotalValues(statement);
+            createTables.createTableClosedTransactions(statement);
+            createTables.createTableOpenTransactions(statement);
+            createTables.createTableDepositsWithdrawals(statement);
 
         }
 
         logger.info("Hello word!");
 
     }
+
 }
---
+++ src/main/java/Interfaces/CreateTableInterface.java
@@ -0,0 +1,31 @@
+package Interfaces;
+
+import java.sql.Statement;
+
+public interface CreateTableInterface {
+
+    static void createTableTotalValues(Statement statement) {
+    }
+
+    ;
+
+    static void createTableDepositsWithdrawals(Statement statement) {
+    }
+
+    ;
+
+    static void createTableOpenTransactions(Statement statement) {
+    }
+
+    ;
+
+    static void createTableClosedTransactions(Statement statement) {
+    }
+
+    ;
+
+    static void createTableAccountBalance(Statement statement) {
+    }
+
+    ;
+}
