640b3d4e9c498d6c78adcec2d9aed4ad7606b4e5
Roman Stetsiuk <romansteciuk021@gmail.com>
2019-08-05 13:06:15 +0200

    Set values from files and set it in accountBalance table in MySQL

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -9,10 +9,17 @@ import java.sql.*;
 
 public class App {
 
+    private static boolean containsNumberValue(String line) {
+        String[] numbersValue = new String[]{"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"};
+        for (String checkLine : numbersValue) {
+            if (line.contains(checkLine)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
-
-        String analizeStatus = "";
-
 
         final Logger logger = Logger.getLogger(App.class);
 
@@ -44,14 +51,17 @@ public class App {
 
         //            Read lines from files
         for (File file : filesList) {
+            int typeAnaliseOperation = 0;
+            String actualDayValue = "";
             if (file.isFile()) {
                 try (BufferedReader bufferedReader = new BufferedReader(new FileReader(file))) {
                     String currentLine;
 
 //                        Get Date report and write this information in LogFile
                     while ((currentLine = bufferedReader.readLine()) != null) {
-                        if (currentLine.contains("End of day")) {
+                        currentLine.trim();
 
+                        if (currentLine.contains("End of day")) {
                             logger.info("\n**********************************************************\n" +
                                     currentLine.trim() +
                                     "\n**********************************************************\n");
@@ -61,7 +71,43 @@ public class App {
                             for (int i = 3; i < splitEndOfDay.length; i++) {
                                 actualDayReport.append(splitEndOfDay[i] + " ");
                             }
-                            String actualDayValue = actualDayReport.toString().trim();
+                            actualDayValue = actualDayReport.toString().trim();
+                        }
+
+                        if (currentLine.contains("Account Balance")) {
+                            typeAnaliseOperation++;
+                        }
+                        if (typeAnaliseOperation == 1 && containsNumberValue(currentLine)) {
+                            String[] splitAccountBalanceValues = currentLine.split("\t");
+                            for (String valueAccountBalance : splitAccountBalanceValues) {
+                                if (!containsNumberValue(valueAccountBalance)) {
+                                    logger.warn("You have wrong value in AccountBalance");
+                                }
+                            }
+                            String balanceAB = splitAccountBalanceValues[0].trim();
+                            String equityAB = splitAccountBalanceValues[1].trim();
+                            String marginAB = splitAccountBalanceValues[2].trim();
+                            String freeMarginAB = splitAccountBalanceValues[3].trim();
+
+                            try (Connection connection = DriverManager.getConnection(connectionUrl, userName, password);
+                                 Statement statement = connection.createStatement()) {
+                                PreparedStatement fieldAccountBalance = connection.prepareStatement(
+                                        "INSERT INTO accountBalance " +
+                                                "(accountBalance_date, balance, equity, margin, freeMargin) VALUES " +
+                                                "(?,?,?,?,?)");
+                                fieldAccountBalance.setString(1, actualDayValue);
+                                fieldAccountBalance.setString(2, balanceAB);
+                                fieldAccountBalance.setString(3, equityAB);
+                                fieldAccountBalance.setString(4, marginAB);
+                                fieldAccountBalance.setString(5, freeMarginAB);
+                                fieldAccountBalance.execute();
+
+                                logger.info(":) OK :)");
+                            }
+                        }
+
+                        if (currentLine.contains("Closed Transaction")) {
+                            typeAnaliseOperation++;
                         }
 
                     }
